8fea862bec481728f5db713d343f526f73144718
JairoLopeznavarretePXL <jairo.lopeznavarrete@student.pxl.be>
2025-05-09 09:03:36 +0200

    ik ben de userpolicy pagina aan het aanpassen

--- screens/UserPolicy.tsx
+++ screens/UserPolicy.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import { View, Text, StyleSheet, Pressable, ScrollView } from 'react-native';
 import { navigate } from '../helpers/RootNavigation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export default function Kabien() {
   const items = [
-    { title: 'Onboarding', screen: 'Onboarding' },
-    { title: 'Tijdbeheer', screen: 'Tijdbeheer' },
+    { title: 'Onboarding', screen: 'Onboarding', icon: 'information-circle-outline'},
+    { title: 'Tijdbeheer', screen: 'Tijdbeheer', icon: 'time-outline' },
     // Voeg hier meer knoppen toe indien nodig
   ];
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <Text style={styles.header}>Gebruikersinstellingen</Text>
+      <Text style={styles.header}>Instellingen en activiteit</Text>
       {items.map((item, index) => (
         <Pressable
           key={index}
           style={styles.button}
           onPress={() => navigate(item.screen)}
         >
-          <Text style={styles.buttonText}>{item.title}</Text>
+         <View style={styles.buttonContent}>
+            <Text style={styles.buttonText}>{item.title}</Text>
+            <Ionicons name={item.icon} size={20} color="#232323" style={styles.icon} />
+         </View>
         </Pressable>
       ))}
     </ScrollView>
@@ -27,23 +31,37 @@ export default function Kabien() {
 
 const styles = StyleSheet.create({
   container: {
-    padding: 20,
+    // padding: 20,
   },
   header: {
+    padding: 20,
     fontSize: 22,
-    fontWeight: 'bold',
-    marginBottom: 20,
+    marginTop: 10,
+    marginBottom: 10,
+    fontFamily: 'Poppins_500Medium',
   },
   button: {
-    backgroundColor: '#007AFF',
-    paddingVertical: 15,
-    paddingHorizontal: 20,
+    width: "100%",
+    borderBottomWidth: 1,
     borderRadius: 10,
     marginBottom: 15,
+    marginVertical: 15,
   },
   buttonText: {
-    color: '#fff',
+    paddingVertical: 20,
+    paddingHorizontal: 40,
+    color: '#232323',
     fontSize: 16,
-    textAlign: 'center',
+    textAlign: 'left',
+    fontFamily: 'Poppins_400Regular',
+  },
+  buttonContent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon: {
+    //marginRight: -10,
+    fontSize: 20,
+    color: '#232323'
   },
 });
